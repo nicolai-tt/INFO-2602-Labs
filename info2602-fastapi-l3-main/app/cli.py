f27237f51dcdd34e9cbf5fa3ead79cddb2dc9a01
nicolai-tt <roccobrooker@gmail.com>
2026-02-21 21:18:32 -0400

    Lab #3 Exercise

---
+++ info2602-fastapi-l3-main/app/cli.py
@@ -0,0 +1,176 @@
+import typer
+from app.database import create_db_and_tables, get_session, drop_all
+from app.models import User, Todo, Category
+from fastapi import Depends
+from sqlmodel import select
+from sqlalchemy.exc import IntegrityError
+
+cli = typer.Typer()
+
+@cli.command()
+def initialize():
+    with get_session() as db: # Get a connection to the database
+        drop_all() # delete all tables
+        create_db_and_tables() #recreate all tables
+        
+        bob = User(username='bob', email='bob@mail.com') # Create a new user (in memory)
+        bob.set_password("bobpass")
+
+        db.add(bob) # Tell the database about this new data
+        db.commit() # Tell the database persist the data
+        db.refresh(bob) # Update the user (we use this to get the ID from the db)
+
+        new_todo = Todo(text='Wash dishes', user_id=bob.id)
+
+        db.add(new_todo) # Tell the database about this new data
+        db.commit() # Tell the database persist the data
+        db.refresh(new_todo) # Update the user (we use this to get the ID from the db)
+
+        print("Database Initialized")
+
+@cli.command()
+def add_task(username:str, task:str):
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).one_or_none()
+        if not user:
+            print("User doesn't exist")
+            return
+        user.todos.append(Todo(text=task))
+        db.add(user)
+        db.commit()
+        print("Task added for user")
+
+@cli.command()
+def toggle_todo(todo_id:int, username:str):
+    with get_session() as db:
+        todo = db.exec(select(Todo).where(Todo.id == todo_id)).one_or_none()
+        if not todo:
+            print("This todo doesn't exist")
+            return
+        if todo.user.username != username:
+            print(f"This todo doesn't belong to {username}")
+            return
+
+        todo.toggle()
+        db.add(todo)
+        db.commit()
+
+        print(f"Todo item's done state set to {todo.done}")
+
+@cli.command()
+def list_todo_categories(todo_id:int, username:str):
+    with get_session() as db: # Get a connection to the database
+        todo = db.exec(select(Todo).where(Todo.id == todo_id)).one_or_none()
+        if not todo:
+            print("Todo doesn't exist")
+        elif not todo.user.username == username:
+            print("Todo doesn't belong to that user")
+        else:
+            print(f"Categories: {todo.categories}")
+
+@cli.command()
+def create_category(username:str, cat_text:str):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).one_or_none()
+        if not user:
+            print("User doesn't exist")
+            return
+
+        category = db.exec(select(Category).where(Category.text== cat_text, Category.user_id == user.id)).one_or_none()
+        if category:
+            print("Category exists! Skipping creation")
+            return
+        
+        category = Category(text=cat_text, user_id=user.id)
+        db.add(category)
+        db.commit()
+
+        print("Category added for user")
+
+@cli.command()
+def list_user_categories(username:str):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).one_or_none()
+        if not user:
+            print("User doesn't exist")
+            return
+        categories = db.exec(select(Category).where(Category.user_id == user.id)).all()
+        print([category.text for category in categories])
+
+@cli.command()
+def assign_category_to_todo(username:str, todo_id:int, category_text:str):
+    with get_session() as db: # Get a connection to the database
+        user = db.exec(select(User).where(User.username == username)).one_or_none()
+        if not user:
+            print("User doesn't exist")
+            return
+        
+        category = db.exec(select(Category).where(Category.text == category_text, Category.user_id==user.id)).one_or_none()
+        if not category:
+            category = Category(text=category_text, user_id=user.id)
+            db.add(category)
+            db.commit()
+            print("Category didn't exist for user, creating it")
+        
+        todo = db.exec(select(Todo).where(Todo.id == todo_id, Todo.user_id==user.id)).one_or_none()
+        if not todo:
+            print("Todo doesn't exist for user")
+            return
+        
+        todo.categories.append(category)
+        db.add(todo)
+        db.commit()
+        print("Added category to todo")
+
+if __name__ == "__main__":
+    cli()
+
+# Exercise
+
+@cli.command()
+def list_todos():
+    with get_session() as db:
+        todos = db.exec(select(Todo)).all()
+
+        if not todos:
+            print("No todos found")
+            return
+
+        for todo in todos:
+            print(
+                f"ID: {todo.id} | "
+                f"Text: {todo.text} | "
+                f"User: {todo.user.username} | "
+                f"Done: {todo.done}"
+            )
+
+
+@cli.command()
+def delete_todo(todo_id: int):
+    with get_session() as db:
+        todo = db.exec(select(Todo).where(Todo.id == todo_id)).one_or_none()
+
+        if not todo:
+            print("Todo doesn't exist")
+            return
+
+        db.delete(todo)
+        db.commit()
+        print("Todo deleted")
+
+
+@cli.command()
+def complete_user_todos(username: str):
+    with get_session() as db:
+        user = db.exec(select(User).where(User.username == username)).one_or_none()
+
+        if not user:
+            print("User doesn't exist")
+            return
+
+        for todo in user.todos:
+            todo.done = True
+            db.add(todo)
+
+        db.commit()
+        print(f"All todos for {username} marked as complete")   
---
+++ info2602-fastapi-l3-main/app/models.py
@@ -0,0 +1,57 @@
+from sqlmodel import Field, SQLModel, Relationship
+from typing import Optional
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
+
+class User(SQLModel, table=True):
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    username:str = Field(index=True, unique=True)
+    email:str = Field(index=True, unique=True)
+    password:str
+
+    todos: list['Todo'] = Relationship(back_populates="user")
+
+    ## Task 3.1 code should go here (special care should go into the indentation)
+
+    ## End of task 3.1 code
+
+    def set_password(self, plaintext_password):
+        self.password = password_hash.hash(plaintext_password)
+
+    def __str__(self) -> str:
+        return f"(User id={self.id}, username={self.username} ,email={self.email})"
+
+# class TodoCategory(SQLModel, table=True):
+#     # Implementation of the TodoCategory model from task 5.1 here
+#     pass
+
+class TodoCategory(SQLModel, table=True):
+    todo_id: int|None = Field(primary_key=True, foreign_key='todo.id')
+    category_id: int|None = Field(primary_key=True, foreign_key='category.id')
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    text: str = Field(max_length=255)
+    done: bool = Field(default=False)
+    # done: bool = False  # <---- can also be written this way if you prefer a pythonic default
+
+    user: User = Relationship(back_populates="todos")
+
+    def toggle(self):
+        self.done = not self.done
+
+    ## Task 3.2 implementation should go here as well. Modify the class like you did for 3.1 above
+
+    ## Task 3.4 implementation should go here as well
+
+    # Task 5.2 code should go here
+    categories: list['Category'] = Relationship(back_populates=("todos"), link_model=TodoCategory)
+    
+class Category(SQLModel, table=True):
+    id: Optional[int] =  Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key='user.id') #set user_id as a foreign key to user.id 
+    text: str = Field(max_length=255)
+
+    todos: list['Todo'] = Relationship(back_populates=("categories"), link_model=TodoCategory)
